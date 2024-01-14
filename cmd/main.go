4b1e84443834179a8966cffa61e07b68204ef768
Griebia <ule2ga@yahoo.com>
2024-01-14 12:53:57 +0200

    Init

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"example.com/Test/handler"
+	"github.com/labstack/echo/v4"
+)
+
+type DB struct{}
+
+func main() {
+	app := echo.New()
+
+	userHandler := handler.UserHandler{}
+	app.GET("/user", userHandler.HandleUserShow)
+	app.GET("/clicked", userHandler.HandleUserShow)
+
+	app.Start(":3000")
+}
---
+++ handler/user.go
@@ -0,0 +1,16 @@
+package handler
+
+import (
+	"example.com/Test/model"
+	"example.com/Test/view/user"
+	"github.com/labstack/echo/v4"
+)
+
+type UserHandler struct{}
+
+func (h UserHandler) HandleUserShow(c echo.Context) error {
+	u := model.User{
+		Email: "a@gg.com",
+	}
+	return render(c, user.Show(u))
+}
